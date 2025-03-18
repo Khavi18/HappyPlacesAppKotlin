f65a89bd214131f4bf0007b573bf95e876948e4f
Khavi18 <khavishini18@gmail.com>
2025-03-18 18:32:14 +0800

    feat: initial commit

---
+++ app/src/main/java/com/example/happyplacesappkotlin/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.happyplacesappkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.happyplacesappkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
